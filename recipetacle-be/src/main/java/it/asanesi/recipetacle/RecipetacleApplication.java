dfd01da18ed89bd77cc1eaf345d31ee5a793ab91
Antonio.Sanesi <anto.sane93@gmail.com>
2024-10-18 17:27:03 +0200

    aggiunti progetti

---
+++ recipetacle-be/src/main/java/it/asanesi/recipetacle/RecipetacleApplication.java
@@ -0,0 +1,13 @@
+package it.asanesi.recipetacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipetacleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipetacleApplication.class, args);
+	}
+
+}
---
+++ recipetacle-be/src/test/java/it/asanesi/recipetacle/RecipetacleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.asanesi.recipetacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipetacleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
